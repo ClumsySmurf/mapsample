efde2e18cd1ddd661d951eaf1354c1d6a52ca0e8
John Hamilton <john@isnapapps.com>
2016-06-22 22:35:35 -0400

    update, remove old pod

--- MapDemo/Pods/Target Support Files/CheapRulerIOS/CheapRulerIOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CheapRulerIOS : NSObject
-@end
-@implementation PodsDummy_CheapRulerIOS
-@end
--- MapDemo/Pods/Target Support Files/CheapRulerIOS/CheapRulerIOS-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double CheapRulerIOSVersionNumber;
-FOUNDATION_EXPORT const unsigned char CheapRulerIOSVersionString[];
-
